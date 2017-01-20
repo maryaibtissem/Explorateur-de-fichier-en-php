50b1f4459a19d616d012a457e2806ff70930d95d
marya <ibtissem.m@codeur.online>
2017-01-20 16:43:07 +0100

    projet fini

--- explorateur.php
+++ explorateur.php
@@ -20,74 +20,110 @@
          
         <header class="col-lg-12"  > <img id="dossier" width="60" src ="dossier-icone-8757-128.png"> <p id="titre"> Explorateur de fichier </p></header>
         <div id="centre">
-            
-        
+           
     <?php
 
         $k="";
+        $k2="";
         $slash = "\\";
     
         $chemin = realpath("explorateur.php"); 
         $chemin = str_replace("explorateur.php","",$chemin);
        
-         
         
-        if(isset($_GET["chemin"])) 
+        
+        if(isset($_GET["chemin"]) && !empty($_GET["chemin"])) 
         { 
+            
+            
+            
             $adresse = $_GET["chemin"];
 //       
             $adresse = str_replace($chemin,"",$adresse);
           
+          
             $phrase= explode("\\",$adresse);
             $phrase = array_filter($phrase);
+            $copier_phrase = $phrase;
+            $fleche= array_pop($copier_phrase);
+            
+            
+            foreach ($copier_phrase as $key => $mo)
+            {
+               $k2 .=$mo.$slash;  
+            }
+            
+            
+            
+            echo '<span id="fleche1"><a  class=" hidden-lg" href="explorateur.php?chemin='.$k2.'"> <i class="fa fa-arrow-left" aria-hidden="true"></i></a></span>';
+            
+           
            //echo $mot[0]; 
            //echo $mot[1];
-            echo'<fieldset><legend><i class="fa fa-folder-open" aria-hidden="true"></i>&nbsp';
+            
+            
+            echo'<fieldset>
+            <legend  class="hidden-xs" ><i class="fa fa-folder-open" aria-hidden="true"></i>&nbsp';
             foreach ($phrase as $key => $mot)
             {
                 if ($mot == end($phrase))
               {
                   
                   $k .=$mot; 
-                  echo '<a href="explorateur.php?chemin='.$k.$slash.'"> '.$mot.' </a>';
+                  echo '<a  href="explorateur.php?chemin='.$k.$slash.'"> '.$mot.' </a>';
                   
               }
                 else
                 {
                     $k .=$mot.$slash; 
-                    echo '<a  href="explorateur.php?chemin='.$k.$slash.'"> '.$mot.'&#8594; </a>';
+                    echo '<a href="explorateur.php?chemin='.$k.$slash.'"> '.$mot.'&#8594; </a> ';
+                    
                 }
         
-               
-            }  
-           echo '</legend><br>';
-           
             
+            }   
+            
+            
+           echo '</legend><br>';
+        
  
-        }
+            }
+        
         else
-        { 
+            { 
             $adresse= $chemin."image".$slash;
             $mot= explode($slash,$adresse);
             $adresse = str_replace($chemin,"",$adresse);
           
               
-
-            echo'<fieldset><legend> <i class="fa fa-folder-open" aria-hidden="true"></i>&nbsp; <a href=" explorateur.php?chemin='.$mot[4].'" " >'.$mot[4].' </a>   </legend><br>';
+          
+            echo'<fieldset><legend> <i class="fa fa-folder-open" aria-hidden="true"></i>&nbsp; <a href=" explorateur.php?chemin='.$mot[4].'" " >'.$mot[4].'<br>
+         
+        </a>   </legend><br>';
   
-        }
+            }
             
+            
+                
         ?>
-    <ul id="menu" class=" col-lg-12 ">
-            <li class="col-lg-4">     Nom   </li>
-            <li class="col-lg-4">    Type   </li>
-            <li class="col-lg-4">   Taille  </li>
+            
+      
+           
+   
+               
+    <ul id="menu" class="hidden-xs col-lg-12 ">
+            <li class="col-lg-4  col-sm-4 col-xs-4">     Nom   </li>
+            <li class="col-lg-4 col-sm-4 col-xs-4">    Type   </li>
+            <li class="col-lg-4 col-sm-4 col-xs-4">   Taille  </li>
                                 
-    </ul>
+    </ul> 
+            <div id="global">
+        
+   
     <?php
        
 //        // print_r($a);
-  
+    
        $a = scandir ($adresse);
         foreach ($a as $Fichier) 
         { 
@@ -97,13 +133,13 @@
                 if ($Fichier != "." && $Fichier != "..") 
                 {  
              
-                    ?> 
-                    <div class="article col-lg-12"> 
+                    ?>    
+                        <div class="article col-lg-12"> 
                     <?php echo '<i class="fa fa-folder" aria-hidden="true"></i> &nbsp; <a href="explorateur.php?chemin='.$adresse.$Fichier.$slash.'"  ">'.$Fichier.'</a> <br> ';
                             
                         ?>
                    
-                    </div> <?php 
+                        </div> <?php 
 
                
                 }
@@ -120,17 +156,17 @@
 //               
                      if($info-> getExtension() =='jpg'|| $info-> getExtension() =='jpeg' || $info-> getExtension() =='png'  )
           
-                        {  ?> 
+                        {  ?>
                             <div class="article"><?php
                           
-                            echo '<div  class="col-lg-4"><i class="fa fa-picture-o" aria-hidden="true"></i> &nbsp;<a  href="'.$adresse.$Fichier.'"> '.$info->getFilename().' </a> </div>'; 
+                            echo '<div  class="col-lg-4  "> <i class="fa fa-picture-o" aria-hidden="true"></i> &nbsp;<a  href="'.$adresse.$Fichier.'"> '.$info->getFilename().' </a> </div>'; 
                                 
-                            echo '<p class="col-lg-4">'.$info-> getExtension().'</p>';
-                            echo '<p class="col-lg-4">'.filesize($info).' octets</p> <br>';   
+                            echo '<p class="col-lg-4  ">'.$info-> getExtension().'</p>';
+                            echo '<p class="col-lg-4 ">'.filesize($info).' octets</p> <br>';   
                                 ?>
                    
                                 
-                            </div> <?php 
+                            </div><?php 
                                  
                         }
                      else 
@@ -141,9 +177,10 @@
                             <?php
                                 
                            
-                                echo '<div  class="col-lg-4"><i class="fa fa-file-text-o" aria-hidden="true"></i> &nbsp;<a  href="telecharger.php?nom_fichier='.$chemin.$adresse.$Fichier.'"> '.$info->getFilename().' </a> </div>  ';  
-                            echo '<p class="col-lg-4">'.$info-> getExtension().'</p>';
-                            echo '<p class="col-lg-4">'.filesize($info).' octets</p> <br>';    
+                                echo '<div  class="col-lg-4 "><i class="fa fa-file-text-o" aria-hidden="true"></i> &nbsp;<a  href="telecharger.php?nom_fichier='.$chemin.$adresse.$Fichier.'"> '.$info->getFilename().' </a> </div>  ';  
+                            echo '<p class="col-lg-4 "> '.$info-> getExtension().'</p>';
+                            echo '<p class="col-lg-4 ">'.filesize($info).' octets</p> <br>';  
+              
                                 
                             ?>
                    
@@ -154,14 +191,34 @@
                                 {
                                     if($info-> getExtension() =='zip')
                                     {?> 
+                                       
                                         <div class="article">
                                         <?php
-                                        echo '<div  class="col-lg-4"><i class="fa fa-file-archive-o" aria-hidden="true"></i> &nbsp;<a  href="telecharger.php?nom_fichier='.$chemin.$adresse.$Fichier.'"> '.$info->getFilename().' </a> </div>  ';  
-                                        echo '<p class="col-lg-4">'.$info-> getExtension().'</p>';
-                                        echo '<p class="col-lg-4">'.filesize($info).' octets</p> <br>';   ?>
+                                        echo ' <div  class="col-lg-4  "><i class="fa fa-file-archive-o" aria-hidden="true"></i> &nbsp;<a  href="telecharger.php?nom_fichier='.$chemin.$adresse.$Fichier.'"> '.$info->getFilename().' </a> </div>  ';  
+                                        echo '<p class="col-lg-4  ">'.$info-> getExtension().'</p>';
+                                        echo '<p class="col-lg-4  ">'.filesize($info).' octets</p> <br>';   ?>
                    
                                 
-                                        </div> <?php
+                </div> <?php
+                                        
+                                        
+                                    }
+                                    else
+                                    {
+                                        if($info-> getExtension() =='PDF')         
+                                        {?> 
+                                       
+                                        <div class="article">
+                                        <?php
+                                            echo '<div  class="col-lg-4  "><i class="fa fa-file-pdf-o" aria-hidden="true"></i> &nbsp;<a  href="'.$adresse.$Fichier.'"> '.$info->getFilename().' </a> </div>  ';  
+                                            echo '<p class="col-lg-4  ">'.$info-> getExtension().'</p>';
+                                            echo '<p class="col-lg-4   ">'.filesize($info).' octets</p> <br>';?>
+                   
+                                
+                </div> <?php
+                                            
+                                        }
+                                        
                                         
                                         
                                     }
@@ -179,8 +236,9 @@
        }
         
    
-    ?>
-        </div>
+    ?> </div>
+        </div>  
+       
         </section>
         </div> 
         <div id="content"></div>
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready (function () {
                   
     $('#content').load('acceuil.php');           
                   
